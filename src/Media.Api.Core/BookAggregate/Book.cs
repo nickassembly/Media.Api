f70dd82a155d11bd88cdfed6e0f4851557e119ad
nickguerra <nickg@maliciousgames.com>
2021-07-20 17:54:01 -0500

    Added Book domain object

---
+++ src/Media.Api.Core/BookAggregate/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Api.Core.BookAggregate
+{
+    public class Book
+    {
+        
+    }
+}
