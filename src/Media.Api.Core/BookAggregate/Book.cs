cdab5319bc70b0e2a64811afaa4e01e67ec1d4b5
nickguerra <nickg@maliciousgames.com>
2021-07-21 18:48:38 -0500

    built out constructor on book aggregate

--- src/Media.Api.Core/BookAggregate/Book.cs
+++ src/Media.Api.Core/BookAggregate/Book.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Ardalis.GuardClauses;
+using Media.Api.SharedKernel;
+using Media.Api.SharedKernel.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,35 @@ using System.Threading.Tasks;
 
 namespace Media.Api.Core.BookAggregate
 {
-    public class Book
+    public class Book : BaseEntity, IAggregateRoot
     {
-        
+        public string Isbn { get; private set; }
+        public string Isbn13 { get; private set; }
+        public List<string> Authors { get; private set; }
+        public string Title { get; private set; }
+        public MediaType MediaType { get; private set; }
+        public string Publisher { get; private set; }
+        public DateTimeOffset PublishDate { get; private set; }
+        public decimal ListPrice { get; private set; }
+        public Guid CreatedBy { get; private set; }
+        public Guid UpdatedBy { get; private set; }
+        public DateTimeOffset CreatedDate { get; private set; }
+        public DateTimeOffset UpdatedDate { get; private set; }
+
+        public Book(string isbn, string isbn13, List<string> authors, string title, MediaType mediaType,
+            string publisher, DateTimeOffset publishDate, decimal listPrice,
+            Guid createdBy, Guid updatedBy, DateTimeOffset createdDate, DateTimeOffset updatedDate)
+        {
+            SetIsbn(isbn);
+        }
+
+        private void SetIsbn(string isbn)
+        {
+            Isbn = Guard.Against.NullOrEmpty(isbn, nameof(isbn));
+
+        }
+
+        // TODO: Add the rest of the Set methods
+
     }
 }
---
+++ src/Media.Api.Core/BookAggregate/MediaType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Api.Core.BookAggregate
+{
+    public enum MediaType
+    {
+        Paperback,
+        Hardcover,
+        Ebook,
+        Audiobook,
+        Other
+    }
+}
