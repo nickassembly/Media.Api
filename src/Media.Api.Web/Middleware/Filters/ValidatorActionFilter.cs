e0caa1353ccc9bd8550a103dc2f296780499bf77
nickguerra <nickg@maliciousgames.com>
2021-07-29 06:03:17 -0500

    Added middleware folder and method stubs

---
+++ src/Media.Api.Web/Middleware/Filters/ValidatorActionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Media.Api.Web.Middleware.Filters
+{
+    public class ValidatorActionFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.ModelState.IsValid)
+            {
+                filterContext.Result = new BadRequestObjectResult(filterContext.ModelState);
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
