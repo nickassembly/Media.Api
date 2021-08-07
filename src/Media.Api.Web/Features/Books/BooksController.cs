52d2cb3371d99f13a06c2de35becd8ecbcb24d67
nickguerra <nickg@maliciousgames.com>
2021-08-07 06:23:36 -0500

    Added stub for create book method, started work on controller

---
+++ src/Media.Api.Web/Features/Books/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using Media.Api.Web.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Api.Web.Features.Books
+{
+    public class BooksController : BaseApiController
+    {
+        private readonly IMediator _mediator;
+
+        public BooksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // POST: api/Books/
+        [HttpPost]
+        [SwaggerOperation(
+            Summary = "Create a book",
+            Description = "Add a new book",
+            OperationId = "Books.Create",
+            Tags = new[] { "Books" })]
+        public async Task<IActionResult> Create([FromBody] BookCreateRequest request)
+        {
+            var response = await _mediator.Send(request);
+
+            return Ok(response.Id);
+        }
+    }
+}
