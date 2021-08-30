ddefa4e272739e7750f7bc320c296f9457ee22a1
nickg <nickg@maliciousgames.com>
2021-08-30 06:58:33 -0500

    Added request and response object for delete book

---
+++ src/Media.Api.Web/Features/Books/Delete/DeleteBookRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Api.Web.Features.Books.Delete
+{
+    public class DeleteBookRequest : IRequest<DeleteBookResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Media.Api.Web/Features/Books/Delete/DeleteBookResponse.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Api.Web.Features.Books.Delete
+{
+    public class DeleteBookResponse : IRequest<DeleteBookResponse>
+    {
+        public int Id { get; set; }
+        public bool IsSuccess { get; set; }
+        public string ToastMessage { get; set; }
+        public string ErrorMessage { get; set; }
+
+    }
+}
