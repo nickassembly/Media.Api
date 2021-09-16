5c959a68fe4b7fc6995a9eb9a8febe12b93f7de9
nickguerra <nickg@maliciousgames.com>
2021-09-16 06:08:02 -0500

    Added code to handle dynamic object in ApiResponse

--- src/Media.Api.SharedKernel/Entities/ApiResponse.cs
+++ src/Media.Api.SharedKernel/Entities/ApiResponse.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Media.Api.SharedKernel.Entities
@@ -65,9 +67,95 @@ namespace Media.Api.SharedKernel.Entities
 
             if (!resultJson.StartsWith("["))
             {
+                try
+                {
+                    dynamic rspBody = JsonSerializer.Deserialize<ExpandoObject>(resultJson);
 
+                    if (!IsPropertyExist(rspBody, "IsSuccess"))
+                    {
+                        if (IsPropertyExist(rspBody, "isSuccess"))
+                            this.IsSuccess = AsBool(rspBody.IsSuccess);
+                    }
+                    else
+                    {
+                        this.IsSuccess = AsBool(rspBody.IsSuccess);
+                    }
+
+                    if (!IsPropertyExist(rspBody, "ToastMessage"))
+                    {
+                        if (IsPropertyExist(rspBody, "toastMessage"))
+                            this.ToastMessage = AsString(rspBody.ToastMessage);
+                    }
+                    else
+                    {
+                        this.ToastMessage = AsString(rspBody.ToastMessage);
+                    }
+
+                    if (!IsPropertyExist(rspBody, "ErrorMessage"))
+                    {
+                        if (IsPropertyExist(rspBody, "ErrorMessage"))
+                            this.ErrorMessage = AsString(rspBody.ErrorMessage);
+                    }
+                    else
+                    {
+                        this.ErrorMessage = AsString(rspBody.ErrorMessage);
+                    }
+
+                    this._result = rspBody;
+                    return;
+                }
+                catch (Exception)
+                {
+                  // Follow into Next block
+                }
+            }
+
+            try
+            {
+                this._result = JsonSerializer.Deserialize<dynamic>(resultJson);
+
+                return;
+            }
+            catch (Exception)
+            {
+
+                this.ToastMessage = resultJson;
+
+                return;
             }
         }
 
+        private static bool IsPropertyExist(dynamic settings, string name)
+        {
+            if (settings is ExpandoObject)
+                return ((IDictionary<string, object>)settings).ContainsKey(name);
+
+            return settings.GetType().GetProperty(name) != null;
+            
+        }
+
+        private static string AsString(dynamic value)
+        {
+            if (value is string)
+                return value;
+            else
+                return value?.GetString();
+        }
+
+        private static bool AsBool(dynamic value)
+        {
+            if (value is bool)
+                return value;
+            else
+                return value?.GetBoolean() ?? false;
+        }
+
+        private static int AsInt(dynamic value)
+        {
+            if (value is int)
+                return value;
+            else
+                return value?.GetInt32() ?? 0;
+        }
     }
 }
--- src/Media.Api.SharedKernel/Middleware/ApiResponseMiddleware.cs
+++ src/Media.Api.SharedKernel/Middleware/ApiResponseMiddleware.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Media.Api.SharedKernel.Middleware
