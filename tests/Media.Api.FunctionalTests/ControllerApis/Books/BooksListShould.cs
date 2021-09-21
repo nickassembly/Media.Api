930e75801523e052beb2cc8827d0242854e4c625
nickguerra <nickg@maliciousgames.com>
2021-09-20 20:05:23 -0500

    Added stub to test list class

--- tests/Media.Api.FunctionalTests/ControllerApis/Books/BooksListShould.cs
+++ tests/Media.Api.FunctionalTests/ControllerApis/Books/BooksListShould.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using Media.Api.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Media.Api.FunctionalTests.ControllerApis.Books
 {
-    public class BooksListShould
+    [Collection("Sequential")]
+    public class BooksListShould : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
-        
+        protected readonly HttpClient _client;
+
+        public BooksListShould(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnBookCollection()
+        {
+
+        }
     }
 }
