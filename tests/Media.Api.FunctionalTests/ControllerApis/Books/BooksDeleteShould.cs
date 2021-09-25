bd6ba400b17aa5df075c27752e722aebc1511725
nickguerra <nickg@maliciousgames.com>
2021-09-25 05:56:38 -0500

    Added test for delete book

--- tests/Media.Api.FunctionalTests/ControllerApis/Books/BooksDeleteShould.cs
+++ tests/Media.Api.FunctionalTests/ControllerApis/Books/BooksDeleteShould.cs
@@ -1,14 +1,81 @@
-﻿using System;
+﻿using Media.Api.Web;
+using Media.Api.Web.Features.Books.Delete;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Media.Api.FunctionalTests.ControllerApis.Books
 {
-    // TODO: Refactor and test delete, test whole api and db 
-    public class BooksDeleteShould
+    [Collection("Sequential")]
+    public class BooksDeleteShould : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
-        
+        protected readonly HttpClient _client;
+
+        public BooksDeleteShould(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task DeleteBook()
+        {
+            var Book1 = new BookDeleteCommand()
+            {
+                Id = 2
+            };
+
+            var jsonString = JsonConvert.SerializeObject(Book1);
+            var bookContent = new StringContent(jsonString.ToString(), Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync($"/api/Books/Delete", bookContent);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<BookDeleteResponse>(stringResponse);
+
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task ThrowExceptionWhenDeleteFails()
+        {
+            var Book1 = new BookDeleteCommand()
+            {
+            };
+
+            var jsonString = JsonConvert.SerializeObject(Book1);
+            var bookContent = new StringContent(jsonString.ToString(), Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync($"/api/Books/Delete", bookContent);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<BookDeleteResponse>(stringResponse);
+
+            Assert.False(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task FailIfBookIsAlreadyDeleted()
+        {
+            var Book1 = new BookDeleteCommand()
+            {
+                Id = 5
+            };
+
+            var jsonString = JsonConvert.SerializeObject(Book1);
+            var bookContent = new StringContent(jsonString.ToString(), Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync($"/api/Books/Delete", bookContent);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<BookDeleteResponse>(stringResponse);
+
+            Assert.False(result.IsSuccess);
+        }
+
     }
 }
